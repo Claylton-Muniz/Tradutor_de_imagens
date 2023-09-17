1c08965bb80f02126aa58eeb04f82d9be1aad485
Claylton-Muniz <claymuniz67@gmail.com>
2023-09-17 14:08:57 -0300

    Adcionando o leito de OCR e atualizando o README

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
 # Tradutor_de_imagens
+
+# Instalações necessárias:
+
+1. PIL (Python Imaging Library):
+
+```powershell
+pip install Pillow
+```
+
+1. **PyTesseract:**
+
+```powershell
+pip install pytesseract
+```
+
+Instale também o Tessetact pelo link: [https://github.com/UB-Mannheim/tesseract/wiki](https://github.com/UB-Mannheim/tesseract/wiki)
+
+- Instale da maneira padrão
+- Certifique-se que o arquivo se encontra na pasta: “C:\Program Files\Tesseract-OCR”
+1. PyAutoGui:
+
+```powershell
+pip install pyautogui
+```
+
+1. Keyboard:
+
+```powershell
+pip install keyboard
+```
--- tradutor.py
+++ tradutor.py
@@ -1,12 +1,25 @@
+from PIL import Image
 from pyautogui import screenshot
+from pytesseract import image_to_string, pytesseract
 from keyboard import add_hotkey, wait
 
 def take_print():
     printscreen = screenshot()
     printscreen.save('print.png')
     print('print tirado com sucesso')
+    imagem = Image.open('print.png')
+    pytesseract.tesseract_cmd = 'C:\\Program Files\\Tesseract-OCR\\tesseract.exe'
+    text = img_to_str(imagem)
+    print(text)
+    
+
+def img_to_str(img):
+    return image_to_string(img)
+    
 
 add_hotkey('ctrl+shift+s', take_print)
 print('Pressione Ctrl + Shift + S para traduzir a imagem')
+
+
 
 wait('esc')
